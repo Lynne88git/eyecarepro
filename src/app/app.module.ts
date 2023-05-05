7ad09c80ca4ddaaff8f0e337db0173819dcbc6f0
Lynne Geldenhuys <geldenhuys.lynne@gmail.com>
2023-05-05 16:31:53 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccountSelectionComponent } from './components/account-selection/account-selection.component';
+import { AccountDetailsComponent } from './components/account-details/account-details.component';
+import { TransactionComponent } from './components/transaction/transaction.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountSelectionComponent,
+    AccountDetailsComponent,
+    TransactionComponent
   ],
   imports: [
     BrowserModule,
